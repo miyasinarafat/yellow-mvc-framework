0d386976fc8681ef2cacb41f6479fccc5416687c
themeyellow <arafat@themeyellow.com>
2018-01-19 23:14:14 +0600

    add limit in function limit method

--- Src/Config/Functions.php
+++ Src/Config/Functions.php
@@ -4,11 +4,11 @@ namespace App\Config;
 
 class Functions
 {
-    public static function limit($value, $lime = 100, $end = '...')
+    public static function limit($value, $limit = 100, $end = '...')
     {
-        if (mb_strwidth($value, 'UTF-8') <= $lime){
+        if (mb_strwidth($value, 'UTF-8') <= $limit){
             return $value;
         }
-        return rtrim(mb_strimwidth($value, 0, $lime, '', 'UTF-8')).$end;
+        return rtrim(mb_strimwidth($value, 0, $limit , '', 'UTF-8')).$end;
     }
 }
